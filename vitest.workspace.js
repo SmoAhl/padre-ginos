cca8962f6821e6beb9ef5a1ca2aef31c7b49357c
Simo Ahlqvist <p19497@edu.sasky.fi>
2025-11-12 13:16:51 +0200

    Testing, unit test with vitest, mocking, testing custom hooks, snapshot testing, test coverage report, vitest ui, browser tests, istanbul

---
+++ vitest.workspace.js
@@ -0,0 +1,27 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  projects: [
+    {
+      extends: "./vite.config.js",
+      test: {
+        // ...
+        // add to the end of the happy-dom test object
+        coverage: {
+          provider: "istanbul",
+          reporter: ["text", "json", "html"],
+        },
+      },
+    },
+    {
+      extends: "./vite.config.js",
+      test: {
+        // ...
+        // add to the end of the browser test object
+        coverage: {
+          reporter: ["text", "json", "html"],
+        },
+      },
+    },
+  ],
+});
